06b3d31a085e74dc3e8241484312637052c71699
MJoe111 <joemoussally111@gmail.com>
2022-05-23 10:18:01 +0300

    Adding manipulation to innerHTML

--- dicee.js
+++ dicee.js
@@ -1,5 +1,6 @@
 const dice1 = document.getElementsByClassName("img1");
 const dice2 = document.getElementsByClassName("img2");
+const title = document.getElementsByTagName("h1")
 var x1; var x2; //variables from dice numbers
 
 //function that runs the game
@@ -9,6 +10,14 @@ const rollDice = () => {
 
     dice1[0].src = "./images/dice"+x1+".png"
     dice2[0].src = "./images/dice"+x2+".png"
+
+    if (x1 > x2) {
+        title[0].innerHTML = "Player 1 Wins!"
+    } else if (x2 > x1) {
+        title[0].innerHTML = "Player 2 Wins!"
+    } else {
+        title[0].innerHTML = "Draw!"
+    }
 }
 
 //function that returns a number between 1 and 6
