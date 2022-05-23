29605752cb3fd28662afa6f50a53dca569b9a0db
MJoe111 <joemoussally111@gmail.com>
2022-05-23 10:11:19 +0300

    Adding rollDice Function

--- dicee.js
+++ dicee.js
@@ -1,2 +1,15 @@
 const dice1 = document.getElementsByClassName("img1");
 const dice2 = document.getElementsByClassName("img2");
+var x1; var x2; //variables from dice numbers
+window.onload = () => {
+    rollDice();
+}
+
+const rollDice = () => {
+    
+}
+
+//function that returns a number between 1 and 6
+const getNumber = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
